6d76a5b6290df3685b0036e948a7fae728548a2b
zhangyihao91 <ryanzhang@gmail.com>
2023-04-25 15:24:11 +0800

    fix onnx model, currently can inference correctly

--- main.cpp
+++ main.cpp
@@ -28,18 +28,16 @@ int main()
     session_options.SetIntraOpNumThreads(1);
 
     // Create a session using the ONNX model
-    Ort::Session session(env, "/home/intellif/workspace/sam4c/encoder-matmul.onnx", session_options);
+    Ort::Session session(env, "/home/intellif/workspace/sam4c/encoder-fix.onnx", session_options);
 
     // Get model input and output names and shapes
     vector<const char*> input_names = {"x"};
     vector<const char*> output_names = {"image_embeddings"};
-    Ort::TypeInfo input_type_info = session.GetInputTypeInfo(0);
-    Ort::TypeInfo output_type_info = session.GetOutputTypeInfo(0);
-    Ort::TensorTypeAndShapeInfo input_tensor_info = input_type_info.GetTensorTypeAndShapeInfo();
-    Ort::TensorTypeAndShapeInfo output_tensor_info = output_type_info.GetTensorTypeAndShapeInfo();
-    vector<int64_t> input_shape = input_tensor_info.GetShape();
-    vector<int64_t> output_shape = output_tensor_info.GetShape();
+    vector<int64_t> input_shape = {1, 3, 1024, 1024};
+    vector<int64_t> output_shape = {1, 256, 64, 64};
     int input_size = 1 * 3 * 1024 * 1024;
+
+    //wcout << "output_shape" << output_shape[0] << output_shape[1] << output_shape[2] << output_shape[3] << endl;
 
     // Load the image using OpenCV
     Mat img = imread("/home/intellif/workspace/sam4c/test.jpg");
@@ -70,26 +68,36 @@ int main()
     // Create input and output tensors
     Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeDefault);
     Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_data, input_size, input_shape.data(), input_shape.size());
-    Ort::Value output_tensor = Ort::Value::CreateTensor<float>(memory_info, nullptr, output_shape[0], output_shape.data(), output_shape.size());
-
     // Run the model
-    std::vector<Ort::Value> output;
-    output = session.Run(Ort::RunOptions{ nullptr }, input_names.data(), &input_tensor, 1, output_names.data(), 1);
+    
+    auto output = session.Run(Ort::RunOptions{ nullptr }, input_names.data(), &input_tensor, 1, output_names.data(), 1);
 
 // Get the output tensor data
-    float* output_data = output[0].GetTensorMutableData<float>();
+    float* output_tensor_data = output[0].GetTensorMutableData<float>();
+    
+    std::vector<int64_t> output_dims(output_shape.begin(), output_shape.end());
+    std::vector<float> output_data(output_dims[0] * output_dims[1] * output_dims[2] * output_dims[3]);
 
-    // Process the output tensor data
-
-    // Create a cv::Mat with the desired shape and copy the output tensor data to it
-    Mat output_mat(1, output_shape[1], CV_32FC(output_shape[2] * output_shape[3]), output_data);
-
-    // Reshape the output matrix to the desired shape
-    output_mat = output_mat.reshape(1, { output_shape[1], output_shape[2], output_shape[3] });
+    std::memcpy(output_data.data(), output_tensor_data, output_data.size() * sizeof(float));
+  // Reshape the output data to a shape of 1x256x64x64 and store it in a vector
+    std::vector<std::vector<std::vector<float>>> output_data_4d(1, std::vector<std::vector<float>>(256, std::vector<float>(64 * 64)));
+    for (int i = 0; i < output_data.size(); ++i) {
+        int n = i / (256 * 64 * 64);
+        int c = (i / (64 * 64)) % 256;
+        int h = (i /64 % 64);
+        int w = i % 64;
+        output_data_4d[n][c][h * 64 + w] = output_data[i];
+        }
+    // Print the first element of the output data
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][0] << std::endl;
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][1] << std::endl;
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][2] << std::endl;
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][3] << std::endl;
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][4] << std::endl;
+    // std::cout << "Output Data [0][0][0]: " << output_data_4d[0][0][5] << std::endl;
 
     // Clean up
     delete[] input_data;
-    cout << output_mat.size() << endl;
+
     return 0;
 }
-    
